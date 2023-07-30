c6beb9d07d2daa8a13eafd479634fd6eda78680a
Daniel100001 <danldplayer2gmail.com>
2023-07-30 15:55:22 +0600

    Project Created

---
+++ app/src/main/java/com/example/firstprojectincleanarchitecture/App.kt
@@ -0,0 +1,5 @@
+package com.example.firstprojectincleanarchitecture
+
+import android.app.Application
+
+class App: Application()
---
+++ app/src/main/java/com/example/firstprojectincleanarchitecture/di/NetworkModule.kt
@@ -0,0 +1,4 @@
+package com.example.firstprojectincleanarchitecture.di
+
+class NetworkModule {
+}
